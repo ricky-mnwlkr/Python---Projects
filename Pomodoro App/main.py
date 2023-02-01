1ed4fe6a0536eaac84ea4d27840fb20b66e40b12
ricky-mnwlkr <56734190+ricky-mnwlkr@users.noreply.github.com>
2023-02-01 06:18:26 +0200

    Update main.py

--- Pomodoro App/main.py
+++ Pomodoro App/main.py
@@ -7,7 +7,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -53,7 +53,7 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     if count > 0:
         global timer
-        timer = window.after(10, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ''
